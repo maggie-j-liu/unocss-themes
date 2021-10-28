cf8850168b539e536a45c886143d4d240e94b966
maggie-j-liu <maggie.j.liu@gmail.com>
2021-10-28 12:23:03 -0700

    simplify

--- index.js
+++ index.js
@@ -1,39 +1,13 @@
-const classMatcher = (themes, theme) => (matcher) => {
-  if (!matcher.startsWith(theme + ":")) {
-    return matcher;
-  }
-  return {
-    matcher: matcher.slice(theme.length + 1),
-    selector: s => `${themes[theme]} ${s}`
-  }
-}
-
-// https://github.com/antfu/unocss/blob/main/packages/core/src/utils/helpers.ts#L3
-const regex = /^\[(.+?)~?="(.*)"\]$/
-
-const attributeMatcher = (themes, theme) => (matcher, raw) => {
-  const match = raw.match(regex);
-  if (!match) {
-    return matcher;
-  }
-  const themeName = match[1];
-  if (themeName !== theme) {
-    return matcher;
-  }
-  return {
-    matcher: match[2],
-    selector: s => `${themes[theme]} ${s}`
-  }
-}
-
-module.exports = function({ themes, attributify = true }) {
+module.exports = function ({ themes }) {
   const themeVariants = Object.keys(themes);
-  const variants = [];
-  themeVariants.forEach(theme => {
-    variants.push(classMatcher(themes, theme));
-    if (attributify) {
-      variants.push(attributeMatcher(themes, theme));
+  const variants = themeVariants.map((theme) => (matcher) => {
+    if (!matcher.startsWith(theme + ":") && !matcher.startsWith(theme + "-")) {
+      return matcher;
     }
-  })
+    return {
+      matcher: matcher.slice(theme.length + 1),
+      selector: (s) => `${themes[theme]} ${s}`,
+    };
+  });
   return variants;
-}
+};
