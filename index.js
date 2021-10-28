06f651bad98bee60c3b0c6a58c7e2940445941a9
maggie-j-liu <maggie.j.liu@gmail.com>
2021-10-27 22:18:02 -0700

    add attributify mode

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
-module.exports = function({ themes }) {
+const classMatcher = (themes, theme) => (matcher) => {
+  if (!matcher.startsWith(theme + ":")) {
+    return matcher;
+  }
+  return {
+    matcher: matcher.slice(theme.length + 1),
+    selector: s => `${themes[theme]} ${s}`
+  }
+}
+
+// https://github.com/antfu/unocss/blob/main/packages/core/src/utils/helpers.ts#L3
+const regex = /^\[(.+?)~?="(.*)"\]$/
+
+const attributeMatcher = (themes, theme) => (matcher, raw) => {
+  const match = raw.match(regex);
+  if (!match) {
+    return matcher;
+  }
+  const themeName = match[1];
+  if (themeName !== theme) {
+    return matcher;
+  }
+  return {
+    matcher: match[2],
+    selector: s => `${themes[theme]} ${s}`
+  }
+}
+
+module.exports = function({ themes, attributify = true }) {
   const themeVariants = Object.keys(themes);
-  const variants = themeVariants.map(theme => {
-    return (matcher) => {
-      if (!matcher.startsWith(theme + ":")) {
-        return matcher;
-      }
-      return {
-        matcher: matcher.slice(theme.length + 1),
-        selector: s => `${themes[theme]} ${s}`
-      }
+  const variants = [];
+  themeVariants.forEach(theme => {
+    variants.push(classMatcher(themes, theme));
+    if (attributify) {
+      variants.push(attributeMatcher(themes, theme));
     }
   })
   return variants;
