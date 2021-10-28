fb1924769247932f673eab3b7d2be28d0d0975e0
maggie-j-liu <maggie.j.liu@gmail.com>
2021-10-27 21:43:58 -0700

    update for unocss 0.2.0

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 module.exports = function({ themes }) {
   const themeVariants = Object.keys(themes);
   const variants = themeVariants.map(theme => {
-    return {
-      match: s => s.startsWith(theme + ":") ? s.slice(theme.length + 1) : null,
-      selector: s => `${themes[theme]} ${s}`
+    return (matcher) => {
+      if (!matcher.startsWith(theme + ":")) {
+        return matcher;
+      }
+      return {
+        matcher: matcher.slice(theme.length + 1),
+        selector: s => `${themes[theme]} ${s}`
+      }
     }
   })
   return variants;
